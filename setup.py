dca201e334e12683478bf80964fef08c0e19f485
Javier <youracow@gmail.com>
2015-03-12 15:54:03 -0600

    added tag and resturcted files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'qrgen',
+  packages = ['qrgen'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Turn a URL into a QR code',
+  author = 'Roy Keys',
+  author_email = 'roy.coding@gmail',
+  url = 'https://github.com/RJ-Skunkworks/qr', # use the URL to the github repo
+  download_url = 'https://github.com/RJ-Skunkworks/qr/tarball/0.1', # I'll explain this in a second
+  keywords = ['qr', 'url', 'qrcode'], # arbitrary keywords
+  classifiers = [],
+)
