95cc07fefb693833065ec54fb9e6ef7eca86ffd1
Javier <youracow@gmail.com>
2015-03-12 18:37:04 -0600

    updated config with accurate info

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'qrgen',
   packages = find_packages(),
-  version = '0.3',
+  version = '0.5',
   description = 'Turn a URL into a QR code',
   author = 'Roy Keys',
   author_email = 'roy.coding@gmail',
-  url = 'https://github.com/RJ-Skunkworks/qr', # use the URL to the github repo
-  download_url = 'https://github.com/RJ-Skunkworks/qr/tarball/0.1', # I'll explain this in a second
-  keywords = ['qr', 'url', 'qrcode'], # arbitrary keywords
+  url = 'https://github.com/RJ-Skunkworks/qr',
+  download_url = 'https://github.com/RJ-Skunkworks/qr/tarball/v0.5',
+  keywords = ['qr', 'url', 'qrcode'], 
   classifiers = [],
+  install_requires=['qrcode'],
 )
